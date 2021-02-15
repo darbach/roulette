b72e88860d9b420fc95a5ae611e07573f04adc20
darbach <danielarbach@gmail.com>
2021-02-15 13:40:59 -0700

    Use view binding.

--- app/src/main/java/edu/cnm/deepdive/roulette/controller/HomeFragment.java
+++ app/src/main/java/edu/cnm/deepdive/roulette/controller/HomeFragment.java
@@ -5,13 +5,11 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
-import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
-import edu.cnm.deepdive.roulette.R;
 import edu.cnm.deepdive.roulette.databinding.FragmentHomeBinding;
 import edu.cnm.deepdive.roulette.viewmodel.HomeViewModel;
 
@@ -29,13 +27,6 @@ public class HomeFragment extends Fragment {
         homeViewModel.spinWheel();
       }
     });
-
-      /*@Override
-      public void onChanged(@Nullable String s) {
-        rouletteValue.setText(s);
-      }
-    });
-    */
     return binding.getRoot();
   }
 
@@ -43,7 +34,6 @@ public class HomeFragment extends Fragment {
   public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
     super.onViewCreated(view, savedInstanceState);
     homeViewModel = new ViewModelProvider(this).get(HomeViewModel.class);
-//    final TextView rouletteValue = root.findViewById(R.id.roulette_value);
     homeViewModel.getRouletteValue().observe(getViewLifecycleOwner(), new Observer<String>() {
       @Override
       public void onChanged(String s) {
