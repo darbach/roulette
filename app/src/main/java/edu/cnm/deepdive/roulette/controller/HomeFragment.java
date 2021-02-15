dda9dbd3ab6feab61b46e2bfdf65b9592e934562
darbach <danielarbach@gmail.com>
2021-02-15 11:34:50 -0700

    Use view binding.

--- app/src/main/java/edu/cnm/deepdive/roulette/controller/HomeFragment.java
+++ app/src/main/java/edu/cnm/deepdive/roulette/controller/HomeFragment.java
@@ -12,30 +12,43 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 import edu.cnm.deepdive.roulette.R;
+import edu.cnm.deepdive.roulette.databinding.FragmentHomeBinding;
 import edu.cnm.deepdive.roulette.viewmodel.HomeViewModel;
 
 public class HomeFragment extends Fragment {
 
   private HomeViewModel homeViewModel;
+  private FragmentHomeBinding binding;
 
   public View onCreateView(@NonNull LayoutInflater inflater,
       ViewGroup container, Bundle savedInstanceState) {
-    homeViewModel =
-        new ViewModelProvider(this).get(HomeViewModel.class);
-    View root = inflater.inflate(R.layout.fragment_home, container, false);
-    final TextView rouletteValue = root.findViewById(R.id.roulette_value);
-    homeViewModel.getRouletteValue().observe(getViewLifecycleOwner(), new Observer<String>() {
-      @Override
-      public void onChanged(@Nullable String s) {
-        rouletteValue.setText(s);
-      }
-    });
-    root.findViewById(R.id.spin_wheel).setOnClickListener(new OnClickListener() {
+    binding = FragmentHomeBinding.inflate(inflater, container, false);
+    binding.spinWheel.setOnClickListener(new OnClickListener() {
       @Override
       public void onClick(View v) {
         homeViewModel.spinWheel();
       }
     });
-    return root;
+
+      /*@Override
+      public void onChanged(@Nullable String s) {
+        rouletteValue.setText(s);
+      }
+    });
+    */
+    return binding.getRoot();
+  }
+
+  @Override
+  public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+    super.onViewCreated(view, savedInstanceState);
+    homeViewModel = new ViewModelProvider(this).get(HomeViewModel.class);
+//    final TextView rouletteValue = root.findViewById(R.id.roulette_value);
+    homeViewModel.getRouletteValue().observe(getViewLifecycleOwner(), new Observer<String>() {
+      @Override
+      public void onChanged(String s) {
+        binding.rouletteValue.setText(s);
+      }
+    });
   }
 }
