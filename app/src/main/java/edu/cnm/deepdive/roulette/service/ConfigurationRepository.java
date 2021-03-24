a3ec383d64502d9d53f148716605bfeca59b3ca5
darbach <danielarbach@gmail.com>
2021-03-23 21:42:21 -0600

    Started configuration structure.

---
+++ app/src/main/java/edu/cnm/deepdive/roulette/service/ConfigurationRepository.java
@@ -0,0 +1,13 @@
+package edu.cnm.deepdive.roulette.service;
+
+import android.content.Context;
+
+public class ConfigurationRepository {
+
+  private final Context context;
+
+  public ConfigurationRepository(Context context) {
+    this.context = context;
+  }
+
+}
